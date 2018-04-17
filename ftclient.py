f8a81de63fbcb13dfd3abd7187a77f88a32ca507
haileybause <38465585+haileybause@users.noreply.github.com>
2018-04-17 10:26:23 -0400

    Update ftclient.py

--- ftclient.py
+++ ftclient.py
@@ -11,11 +11,19 @@ def parseArgs():
   parser.add_argument('--server')
   #mutually exclusive group to determine if in send or receive mode
   group = parser.add_mutually_exclusive_group()
-  group.add_argument('--receive', action = 'store_true', nargs = '+')
+  group.add_argument('--receive', action = 'store_true')
   group.add_argument('--send', action = 'store_true', nargs = '+')
+  
   arg = parser.parse_args()
   server = arg.server
   receive = arg.receive
   send = arg.send
-  #returns argumente
+  #returns arguments
   return server, receive, send
+
+#gets all command line arguments and sets to corresponding variables
+server, receive, send = parseArgs()
+if(send):
+  ID = send[0]
+  filename = send[1]
+  
